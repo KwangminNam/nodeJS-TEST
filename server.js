d75e4424bdd9a843e02217d00fab6dee7bde72b7
kwangminNam <johnpower@naver.com>
2022-09-25 19:58:54 +0900

    mongo db

--- server.js
+++ server.js
@@ -40,18 +40,35 @@ app.get('/write',(요청,응답)=>{
 
 app.post('/add',(req,res)=>{
   res.send('complete!')
-  db.collection('post').insertOne({
-    제목:req.body.title,
-    날짜:req.body.date
-  },(에러,결과)=>{
-    console.log('저장완료');
-  });
+  db.collection('counter').findOne({name :'게시물갯수'} , (에러,결과)=>{
+    console.log(결과.totalPost);
+    var totalPostNum = 결과.totalPost;
+
+    db.collection('post').insertOne({
+      _id:totalPostNum + 1,
+      제목:req.body.title,
+      날짜:req.body.date
+    },(에러,결과)=>{
+      console.log('저장완료');
+      db.collection('counter').updateOne({name:"게시물갯수"},{$inc : {totalPost:1}},(err,res)=>{
+        if(err) return console.log(err);
+      } )
+    });
+  })
 })
 
 app.get('/list',(req,res)=>{
   db.collection('post').find().toArray((error,result)=>{
     console.log(result);
     res.render('list.ejs' ,{ posts : result} );
+  })
+})
+
+app.delete('/delete' , function(req,res){
+  console.log(req.body)
+  req.body._id = parseInt(req.body._id);
+  db.collection('post').deleteOne(req.body,(err,result)=>{
+    console.log('삭제완료!')
   })
 })
  
